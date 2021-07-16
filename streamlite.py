acb9460e64b5ffba5d25c515361e7a633d07bb44
Ifelolu David <48281015+IfeloluDavid@users.noreply.github.com>
2021-07-16 14:40:58 +0100

    Update streamlite.py

--- streamlite.py
+++ streamlite.py
@@ -1,13 +1,6 @@
 import streamlite as st
 
-# Add a selectbox to the sidebar:
-add_selectbox = st.sidebar.selectbox(
-    'How would you like to be contacted?',
-    ('Email', 'Home phone', 'Mobile phone')
-)
-
-# Add a slider to the sidebar:
-add_slider = st.sidebar.slider(
-    'Select a range of values',
-    0.0, 100.0, (25.0, 75.0)
-)
+import streamlit as st
+st.title("Face Mask Detection using Machine Learning")
+st.header("Face Mask Detection using Machine Learning")
+st.text("Upload an Image for image classification as with_mask or without_mask")
